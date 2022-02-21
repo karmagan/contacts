bfe8dec20b074c69732df580e20fe11f5256027e
Kadir Armagan <kadirarmagan@gmail.com>
2022-02-21 23:39:02 +0000

    modify ContactsAPI

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,11 +1,7 @@
 const api = "http://localhost:3300";
 
-let token = localStorage.token;
-if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
-
 const headers = {
   Accept: "application/json",
-  Authorization: token,
 };
 
 export const getAll = () =>
