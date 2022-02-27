a54b3535eb1f3c2d7a5013a7e1d7847b0e410e7a
Kadir Armagan <kadirarmagan@gmail.com>
2022-02-27 15:59:29 +0000

    Select all fix

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,12 +2,12 @@ import React from "react";
 import Contact from "./Contact";
 
 function ContactsList(props) {
-  const { contacts, selectContact, selectedContacts } = props;
+  const { contacts, selectContact, selectedContacts, selectedAll } = props;
 
   return (
     <div>
       {contacts.map((contact) => (
-            <Contact onClick={()=>selectContact(contact)} key={contact.id} contact={contact} selected={selectedContacts.filter(s=>s.id===contact.id).length===1} />
+            <Contact onClick={()=>selectContact(contact)} key={contact.id} contact={contact} selected={selectedContacts.filter(s=>s.id===contact.id).length>0 || selectedAll} />
       ))}
     </div>
   );
--- src/components/ContactsPage.js
+++ src/components/ContactsPage.js
@@ -9,7 +9,9 @@ class ContactsPage extends React.Component {
     selectedContacts: [],
     query: "",
     skip: 0,
+    skipSelected: 0,
     loading: false,
+    selectedAll: false,
   };
 
   //Initial loading of first 10 contacts
@@ -34,11 +36,31 @@ class ContactsPage extends React.Component {
     }
   };
 
+  scrollSelectedHandler = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom && this.state.selectedAll) {
+      this.setState(
+        { skipSelected: this.state.skipSelected + 10, loading: true },
+        () => this.selectAll()
+      );
+    }
+  };
+
   //Get the contacts from the server
   searchContacts = () => {
     ContactsAPI.search(this.state.query, this.state.skip).then((contacts) => {
       this.setState({
         contacts: [...this.state.contacts, ...contacts],
+        loading: false,
+      });
+    });
+  };
+
+  selectAll = () => {
+    ContactsAPI.search("", this.state.skipSelected).then((contacts) => {
+      this.setState({
+        selectedContacts: [...this.state.selectedContacts, ...contacts],
         loading: false,
       });
     });
@@ -56,39 +78,59 @@ class ContactsPage extends React.Component {
     }
   };
 
-  //Add all the contacts from the server to the selectedContacts list
-  selectAll = () => {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({
-        selectedContacts: contacts,
-      });
-    });
+  // Toggle select all state
+  toggleSelectAll = () => {
+    this.setState(
+      {
+        selectedAll: !this.state.selectedAll,
+        selectedContacts: [],
+        skipSelected: 0,
+      },
+      () => {
+        if (this.state.selectedAll === true) {
+          this.selectAll();
+        }
+      }
+    );
   };
 
   render() {
     return (
       <div className="contacts-page">
-        <div className="contacts-pane">
+        <div className="contacts">
           <ContactsSearch searchHandler={this.searchHandler} />
           <h1>Contacts</h1>
           <h3>Showing {this.state.contacts.length} contacts</h3>
-          <button onClick={this.selectAll}>Select All</button>
+          <button onClick={this.toggleSelectAll}>
+            {this.state.selectedAll === true ? `Deselect All` : `Select All`}
+          </button>
           <div className="contacts-list " onScroll={this.scrollHandler}>
             <ContactsList
               selectContact={this.selectContact}
               selectedContacts={this.state.selectedContacts}
               contacts={this.state.contacts}
+              selectedAll={this.state.selectedAll}
             />
           </div>
         </div>
 
-        <div className="selected-contacts">
+        <div className="contacts">
           <h1>Selected Contacts</h1>
-          <ContactsList
-            selectContact={this.selectContact}
-            selectedContacts={this.state.selectedContacts}
-            contacts={this.state.selectedContacts}
-          />
+          <h3>
+            Selected{" "}
+            {this.state.selectedAll === true
+              ? "all"
+              : this.state.selectedContacts.length}{" "}
+            contacts
+          </h3>
+          <div className="contacts-list" onScroll={this.scrollSelectedHandler}>
+            <ContactsList
+              selectContact={this.selectContact}
+              selectedContacts={this.state.selectedContacts}
+              contacts={this.state.selectedContacts}
+              selectedAll={this.state.selectedAll}
+            />
+          </div>
         </div>
         <div className={this.state.loading ? "loading" : "loaded"}></div>
       </div>
